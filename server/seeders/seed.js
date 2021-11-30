93cb6e7f4ce8a63fa914cf34b1a96afaeca1a8cf
Carson Johnson <carson74johnson@gmail.com>
2021-11-30 14:57:29 -0500

    added mongoose connection to seed.js

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,2 +1,10 @@
 const mongoose = require('mongoose');
 const db = require('../models');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/biiggie', 
+{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+});
