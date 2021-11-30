b4ddbe9f2c77ede2dca43532ee708aef38770ba1
Femi Ogun <femiogun21@yahoo.com>
2021-11-30 14:48:56 -0500

    Added Typedefs for User, Query, and Biiggie

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,25 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+    }
+
+    type Query {
+        user: User
+    }
+
+    type Biiggie {
+        title: String
+        deadline: Date
+        description: String
+        sources: Array
+        images: String
+        helpOptions: [helpOption]!
+    }
+    `;
+
+    module.exports = typeDefs;
