22630f35132b93d864c9b7180ebccbc41b2b39c5
Carson Johnson <carson74johnson@gmail.com>
2021-11-30 15:36:19 -0500

    added helpOptions to test Biiggie in seed.js

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -15,6 +15,24 @@ const biiggieSeed = [
         createdAt: new Date(new Date().setDate(new Date().getDate())),
         deadline: new Date(new Date().setDate(new Date().getDate() + 9)),
         description: 'My Biiggie that I I need help getting my dream taco food truck business off the ground. I serve all types of tacos and have worked very hard renovating an old truck I but. Any help is welcome!',
-        images: ['https://mobile-cuisine.com/wp-content/uploads/2015/09/food-truck-branding.jpg']
-    }
+        images: ['https://mobile-cuisine.com/wp-content/uploads/2015/09/food-truck-branding.jpg'],
+        helpOptions: [
+            {
+                name: 'Graphic Designer',
+                description: 'A graphic designer to help design the truck.',
+                numOfPeople: 1,
+            },
+            {
+                name: 'Vehicle Wrap Installer',
+                description: 'A vehible wrap artist to install a wrap on the truck.',
+                numOfPeople: 1,
+            },
+            {
+                name: 'Marketer',
+                description: 'Help advertise the business.',
+                numOfPeople: 3,
+            },
+        ],
+    },
 ]
+
