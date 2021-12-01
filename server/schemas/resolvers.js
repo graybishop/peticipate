f09fbf23fc9b12ed8b44788bb34d4a86f05524a9
Carson Johnson <carson74johnson@gmail.com>
2021-12-01 13:22:35 -0500

    added a userById Query to the resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,14 @@
-const Biiggie = require("../models/Biiggie.js");
+const Biiggie = require('../models/Biiggie.js');
+const User = require('../models/User');
 
 const resolvers ={
   // Temp resolver for server testing
   Query:{
     user: ()=>{
       return {message: 'you got a user'}
+    },
+    userById: async ({ _id }) => {
+      return await User.findOne({ _id });
     },
     biiggies: async ()=>{
       return await Biiggie.find({})
