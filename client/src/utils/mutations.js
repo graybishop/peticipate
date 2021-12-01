608d59dcf198063dade58278bf8bc1cbfbd20b09
Carson Johnson <carson74johnson@gmail.com>
2021-12-01 16:28:43 -0500

    finished user create using submit form

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,26 +1,49 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const CREATE_BIIGGIE = gql`
-mutation createBiiggie(
-    $title: String!,
-    $deadline: Int!,
-    $description: String!,
-    $sources: [String],
+  mutation createBiiggie(
+    $title: String!
+    $deadline: Int!
+    $description: String!
+    $sources: [String]
     $images: String
+  ) {
+    createBiiggie(
+      title: $title
+      deadline: $deadline
+      description: $description
+      sources: $sources
+      images: $images
     ) {
-        createBiiggie(
-            title: $title,
-            deadline: $deadline,
-            description: $description,
-            sources: $sources,
-            images: $images
-            ) {
-                _id
-                title
-                deadline
-                description
-                sources
-                images
-            }
+      _id
+      title
+      deadline
+      description
+      sources
+      images
     }
-`
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation Mutation(
+    $firstName: String!
+    $lastName: String
+    $email: String
+    $password: String
+    $username: String
+  ) {
+    newUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+      username: $username
+    ) {
+      username
+      email
+      firstName
+      lastName
+    }
+  }
+`;
