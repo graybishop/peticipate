1ca72489916d5c7d3b67ade665804ba048f4ed3a
Jake Swackhammer <j.100101@gmail.com>
2021-12-01 16:38:49 -0500

    added clientside context to authoration header

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
 import Home from "./pages/HomePage/index.js";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/index.js";
 import Signup from "./pages/Signup/index.js";
 import Header from "./components/PageComponents/Header.js";
 import { NewBiiggieForm } from "./components/PageComponents/NewBiiggieForm.js";
 import TestPage from "./pages/TestPage/index.js";
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: "/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
