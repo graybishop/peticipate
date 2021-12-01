8c38227fe15d40d938c85a7add0ca78d030e3626
Jake Swackhammer <j.100101@gmail.com>
2021-12-01 16:16:56 -0500

    created new query to test context

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server-errors');
 const Biiggie = require('../models/Biiggie.js');
 const User = require('../models/User');
 
@@ -12,6 +13,13 @@ const resolvers ={
     biiggies: async ()=>{
       return await Biiggie.find({})
     },
+    authBiggiesReq: async (parent, args, context)=>{
+      console.log('Context looks like this:', context.user)
+      if(!context.user){
+        throw new AuthenticationError('You need to be logged in to pull these Biggies')
+      }
+      return await Biiggie.find({})
+    }
   },
   Mutation: {
     newUser: async (parent, args) => {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -41,6 +41,7 @@ const typeDefs = gql`
     type Query {
         user(_id: String): User
         biiggies: [Biiggie]
+        authBiggiesReq: [Biiggie]
     }
 
     type Mutation {
