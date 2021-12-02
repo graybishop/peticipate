65ed24902961dc27fcdb4b8c6fbabf8afff2b602
Jake Swackhammer <j.100101@gmail.com>
2021-12-02 12:08:54 -0500

    created hero section, added colors to tailiwnd theme

--- client/src/components/PageComponents/Header.js
+++ client/src/components/PageComponents/Header.js
@@ -1,19 +1,17 @@
 import { Link } from "react-router-dom";
-import { FaBeer } from 'react-icons/fa';
-import { ImSearch, ImUser } from 'react-icons/im';
 import {FiUser, FiSearch, FiPlus} from 'react-icons/fi'
 
 
 const Header = () => {
   return (
-    <div className='container mx-auto flex flex-row items-center justify-between py-6 px-6 bg-blue-50'>
+    <div className='container mx-auto flex flex-row items-center justify-between py-6 px-6 bg-blue-header'>
       {/* <div>
         <h1 className='text-4xl'>Ende<span className='text-sm underline align-top transform scale-150'>a</span><span className='text-purple-700'>V</span>or</h1>
       </div> */}
       <div>
-        <h1 className='text-4xl font-extrabold text-yellow-600'><Link to="/">Biiggie</Link></h1>
+        <h1 className='text-4xl font-extrabold text-orange-primary'><Link to="/">Biiggie</Link></h1>
       </div>
-      <div className='flex flex-row items-center gap-2 text-3xl text-blue-500'>
+      <div className='flex flex-row items-center gap-2 text-3xl text-blue-nav-button'>
         {/* <Link to="/">Home Page</Link> */}
         {/* <Link to="/login">Login Page</Link> */}
         {/* <Link to="/sign-up">Sign Up</Link> */}
