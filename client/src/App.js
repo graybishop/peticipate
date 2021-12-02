ecc0a4d05f1a5b321b40f409511ec9f31a95c804
Femi Ogun <femiogun21@yahoo.com>
2021-12-02 12:30:38 -0500

    Created profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Home from "./pages/HomePage/index.js";
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Profile from "./pages/ProfilePage/index.js";
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -12,6 +13,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Home/>
+      <Profile/>
     </ApolloProvider>
   );
 }
