3f0ffbcde9a0cc2ffc7164f037fd990330f7c86a
Femi Ogun <femiogun21@yahoo.com>
2021-12-03 14:25:21 -0500

    Changed class to className

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -48,12 +48,12 @@ function Signup(props) {
                 <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
       {/* <Link to="/login">← Go to Login</Link> */}
 
-      <h1 class="mb-8 text-3xl text-center font-bold">Sign Up</h1>
+      <h1 className="mb-8 text-3xl text-center font-bold">Sign Up</h1>
       <form onSubmit={handleFormSubmit}>
         <div>
         <label htmlFor="firstName"></label>
           <input
-            class="block border border-grey-light w-full p-3 rounded mb-4"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
             placeholder="First Name"
             name="firstName"
             type="text"
@@ -62,7 +62,7 @@ function Signup(props) {
           />
         <label htmlFor="lastName"></label>
           <input
-            class="block border border-grey-light w-full p-3 rounded mb-4"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
             placeholder="Last Name"
             name="lastName"
             type="text"
@@ -71,7 +71,7 @@ function Signup(props) {
           />
           <label htmlFor="username"></label>
           <input
-            class="block border border-grey-light w-full p-3 rounded mb-4"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
             placeholder="Username"
             name="username"
             type="text"
@@ -80,7 +80,7 @@ function Signup(props) {
           />
           <label htmlFor="email"></label>
           <input
-            class="block border border-grey-light w-full p-3 rounded mb-4"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
             placeholder="Email"
             name="email"
             type="email"
@@ -89,7 +89,7 @@ function Signup(props) {
           />
           <label htmlFor="pwd"></label>
           <input
-            class="block border border-grey-light w-full p-3 rounded mb-4"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
             placeholder="Password"
             name="password"
             type="password"
@@ -105,11 +105,11 @@ function Signup(props) {
             onChange={handleChange}/> */}
         <button
             type="submit"
-            class="bg-orange-primary hover:bg-orange-hover text-white font-bold py-3 my-1 rounded text-center w-full"
+            className="bg-orange-primary hover:bg-orange-hover text-white font-bold py-3 my-1 rounded text-center w-full"
             >Create Account</button>
         </div>
       </form>
-      <div class="text-grey-dark mt-6">
+      <div className="text-grey-dark mt-6">
             Already have an account?&nbsp;
             <Link class="no-underline border-b text-orange-primary" to="/login">
             Log in
