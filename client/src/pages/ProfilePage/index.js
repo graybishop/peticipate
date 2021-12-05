3aa41b1556b07d0656a21369db846793ee67fff6
Carson Johnson <carson74johnson@gmail.com>
2021-12-05 15:21:40 -0500

    created profileBiiggieCard" "

--- client/src/pages/ProfilePage/index.js
+++ client/src/pages/ProfilePage/index.js
@@ -5,7 +5,7 @@ import { QUERY_ME } from '../../utils/queries';
 const Profile = () => {
     const { loading, data } = useQuery(QUERY_ME)
     const user = data?.me || data?.user || {};
-    if (loading) return <p>Loading ...</p>;
+    
     return(
         <div className="text-center box-border p-4 border-4 m-4 mx-auto">
             <img className="object-contain rounded-full border shadow max-h-60 mx-auto" id="profileImage"          src="https://source.unsplash.com/featured/1000x1000/?profile" alt="user profile" />
@@ -17,56 +17,28 @@ const Profile = () => {
             <div>
             <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
     {/* <!--Card 1--> */}
-    <button className="btn">
-    <div className="rounded overflow-hidden shadow-lg">
-      <img className="w-full" src="" alt="Biiggie"/>
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Biiggie 1</div>
-        <p className="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
-        </p>
+    {/* if (loading) { return (<p>Loading ...</p>)} */}
+
+    {user.createdBiiggies.map((biiggie) => {
+      return <button className="btn">
+      <div className="rounded overflow-hidden shadow-lg">
+        <img className="w-full" src="" alt="Biiggie"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{biiggie.title}</div>
+          <p className="text-gray-700 text-base">
+            {biiggie.description}
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Food</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Politics</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Education</span>
+        </div>
       </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Food</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Politics</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Education</span>
-      </div>
-    </div>
-    </button>
-    {/* <!--Card 2--> */}
-    <button className="btn">
-    <div className="rounded overflow-hidden shadow-lg">
-      <img className="w-full" src="" alt="Biiggie"/>
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Biiggie 2</div>
-        <p className="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
-        </p>
-      </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Food</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Politics</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Education</span>
-      </div>
-    </div>
-    </button>
-    {/* <!--Card 3--> */}
-    <button className="btn">
-    <div className="rounded overflow-hidden shadow-lg">
-      <img className="w-full" src="" alt="Biiggie"/>
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Biiggie 3</div>
-        <p className="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
-        </p>
-      </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Food</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Politics</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Education</span>
-      </div>
-    </div>
-    </button>
+      </button>
+    })}
+    
+    
   </div>
 </div>
 <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">
