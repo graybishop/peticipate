d0f897223bccc4c5bb4cf9aef0552e9d0741b7ee
Jake Swackhammer <j.100101@gmail.com>
2021-12-06 01:01:47 -0500

    added percentage completed to each goal

--- client/src/pages/Biiggie Page/index.js
+++ client/src/pages/Biiggie Page/index.js
@@ -20,6 +20,14 @@ const HelpOptionCard = ({ helpOption, userId }) => {
     moneyGoal = true;
   }
 
+  const percentageDone = () => {
+    if (moneyGoal) {
+      return Math.floor((1 - (moneyRemaining / helpOption.moneyRequested)) * 100);
+    }
+
+    return Math.floor((1 - (numberOfPeopleRemaining / helpOption.numOfPeople)) * 100);
+  };
+
   useEffect(() => {
     const isUserCommitted = () => {
       let userHasCommit = false;
@@ -47,6 +55,8 @@ const HelpOptionCard = ({ helpOption, userId }) => {
     if (amount) {
       mutationVars.moneyCommitted = amount;
       setMoneyRemaining(moneyRemaining - amount);
+    } else {
+      setNumberOfPeopleRemaining(numberOfPeopleRemaining - 1);
     }
     try {
       await addToHelpOption({
@@ -79,10 +89,18 @@ const HelpOptionCard = ({ helpOption, userId }) => {
         <p className=''>{helpOption.description}</p>
       </div>
       <div>
-        Currently % of the way there with {moneyGoal ? `$${moneyRemaining} left to go.` : `${numberOfPeopleRemaining} ${numberOfPeopleRemaining === 1 ? 'person' : 'people'} still needed.`}
+        Currently {percentageDone()}% of the way there with {moneyGoal ?
+          `$${moneyRemaining} left to go.` :
+          `${numberOfPeopleRemaining} ${numberOfPeopleRemaining === 1 ?
+            'person' : 'people'} still needed.`}
       </div>
       <div>
-        {userCommittedState ? (<div>Thanks for your commitment!</div>) : moneyGoal ? moneyButtons : peopleButton}
+        {userId === null ?
+          (<p>You need to be logged-in to help!</p>) :
+          userCommittedState ?
+            (<p>Thanks for your commitment!</p>) :
+            moneyGoal ?
+              moneyButtons : peopleButton}
       </div>
     </div>
   );
@@ -104,23 +122,28 @@ const BiiggiePage = ({ biiggie }) => {
 
   return (
     <div className='bg-body-background-blue'>
-      <div className="container mx-auto flex flex-col p-4 gap-4 filter drop-shadow items-center">
-        <div className='border-l border-blue-secondary pl-2'>
-          <h1 className="text-4xl">{data.title}</h1>
-          <div className='flex flex-row items-center gap-2'>
-            <img className="object-cover rounded-full border-2 shadow h-20 w-20 border-blue-secondary"
-              src={data.createdBy.image} alt='profile' />
-            <div>
-              <p className="font-bold">{data.createdBy.firstName} {data.createdBy.lastName}</p>
-              <p className="italic">{data.createdBy.username}</p>
-            </div>
+      <img className='' src={data.images} alt="" />
+      <div className='flex flex-col items-center gap-2'>
+        <h1 className="text-4xl px-2 text-center w-max py-1 bg-blue-nav-button rounded-b shadow text-white">{data.title}</h1>
+        <div className='flex flex-row items-center gap-2'>
+          <img className="object-cover rounded-full border-2 shadow h-20 w-20 border-blue-secondary"
+            src={data.createdBy.image} alt='profile' />
+          <div>
+            <p className="font-bold">{data.createdBy.firstName} {data.createdBy.lastName}</p>
+            <p className="italic">{data.createdBy.username}</p>
           </div>
         </div>
-        <img className='shadow rounded' src={data.images} alt="" />
-        <div className='border-t border-blue-secondary '>
+      </div>
+      <div className="container mx-auto flex flex-col p-4 gap-4 filter drop-shadow items-center">
+        <div className='flex flex-col items-center'>
+          <h2 className='px-2 text-2xl text-center w-max py-1 bg-blue-nav-button rounded text-white  shadow'>Description</h2>
           <p className="p-2 ">{data.description}</p>
         </div>
         <div className='flex flex-col gap-2'>
+          <div className='flex flex-col items-center'>
+            <h2 className='px-2 text-2xl text-center w-max py-1 bg-blue-nav-button  text-white rounded-b shadow'>Ways to help</h2>
+            <p className='p-2'>You can help make this Biiggie a reality by committing to help make these goals come true.</p>
+          </div>
           {mappedHelpOptions}
         </div>
         <p className="text-2xl text-center text-orange-hover font-semibold animate-pulse">{Math.floor((new Date(data.deadline) - new Date()) / 1000 / 86400)} Days Left</p>
