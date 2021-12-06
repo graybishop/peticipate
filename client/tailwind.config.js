8806b6ca07ee851a3cbb2b7d7dd6e1841ac90c75
JC Renner <jc-renner@lu-tec.com>
2021-12-05 23:17:04 -0500

    lots of updates to add Biiggie walkthru - copy, buttons, more styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,9 +7,11 @@ module.exports = {
         test: '#293847',
         'orange-primary': '#FF6F00',
         'orange-hover': '#EA580C',
+        'orange-light': '#FFB780',
         'blue-secondary': '#8FD3FF',
         'blue-header' : '#D9F0FF',
         'blue-nav-button':'#009BFF',
+        'blue-hover' :'#0073BF',
         'body-background-blue': '#F1F9FF'
       }
     },
