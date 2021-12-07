54bec4fbe2c33e49eeb61bac61607d3c8539bf10
Carson Johnson <carson74johnson@gmail.com>
2021-12-06 22:22:19 -0500

    imported in BiiggieCard from HomePage folder

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useQuery } from '@apollo/client';
 import { GET_BIIGIES, GET_KEYWORDS } from '../../utils/queries.js';
 import { Link } from "react-router-dom";
+import { BiiggieCard } from "../../pages/HomePage/BiiggieCard";
 
 const SearchPage = () => {
     const { data: biiggiesData, error: biiggiesError } = useQuery(GET_BIIGIES);
@@ -11,6 +12,8 @@ const SearchPage = () => {
     console.log(keywordsData);
 
     document.title = 'Search || Search Keywords';
+
+    
 }
 
 export default SearchPage;
