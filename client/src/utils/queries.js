8916d4b562c5e5ec932be5e9dd7174f851ce8649
Carson Johnson <carson74johnson@gmail.com>
2021-12-06 22:11:34 -0500

    added GET_KEYWORDS query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,116 +1,157 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const GET_BIIGIES = gql`
-query GiveMeTheBiiggies {
-  biiggies {
-    _id
-    title
-    deadline
-    description
-    sources
-    images
-    likes
-    createdBy{
-      _id
-      firstName
-      lastName
-      username
-      image
-    }
-    helpOptions {
-      name
-      description
-      numOfPeople
-      moneyReceived
-      moneyRequested
-      registeredUsers{
-        _id
-      }
-    }
-  }
-}
-`;
-
-export const AUTH_BIGGIES = gql`
-query AuthBiggiesReq {
-  authBiggiesReq {
-    title
-    deadline
-    description
-  }
-}
-`;
-
-export const BIIGGIE = gql`
-query Biiggie($id: ID) {
-  biiggie(_id: $id) {
-    _id
-    title
-    deadline
-    description
-    sources
-    images
-    likes
-    createdBy{
-      _id
-      username
-      firstName
-      lastName
-      image
-    }
-    keywords {
-      keyword
-    }
-    helpOptions {
-      _id
-      name
-      description
-      numOfPeople
-      registeredUsers {
-        _id
-        username
-      }
-      moneyRequested
-      moneyReceived
-    }
-    comments {
-      author {
-        username
-        image
-        firstName
-        lastName
-      }
-      body
-    }
-  }
-}
-`;
-
-export const QUERY_ME = gql`
-query Query {
-  me {
-    _id
-    username
-    email
-    firstName
-    lastName
-    firstName
-    image
-    createdBiiggies {
+  query GiveMeTheBiiggies {
+    biiggies {
       _id
       title
       deadline
       description
       sources
       images
+      likes
+      createdBy {
+        _id
+        firstName
+        lastName
+        username
+        image
+      }
       helpOptions {
         name
         description
         numOfPeople
-        moneyRequested
         moneyReceived
+        moneyRequested
+        registeredUsers {
+          _id
+        }
       }
     }
   }
-}
+`;
+
+export const GET_KEYWORDS = gql`
+  query Query {
+    keywords {
+      keyword
+      biiggie {
+        _id
+        title
+        likes
+        deadline
+        description
+        sources
+        images
+        helpOptions {
+          _id
+          name
+          description
+          numOfPeople
+          registeredUsers {
+            _id
+            username
+            email
+            firstName
+            lastName
+            image
+          }
+          moneyRequested
+          moneyReceived
+        }
+        createdBy {
+          _id
+          username
+          email
+          firstName
+          lastName
+          image
+        }
+      }
+    }
+  }
+`;
+
+export const AUTH_BIGGIES = gql`
+  query AuthBiggiesReq {
+    authBiggiesReq {
+      title
+      deadline
+      description
+    }
+  }
+`;
+
+export const BIIGGIE = gql`
+  query Biiggie($id: ID) {
+    biiggie(_id: $id) {
+      _id
+      title
+      deadline
+      description
+      sources
+      images
+      likes
+      createdBy {
+        _id
+        username
+        firstName
+        lastName
+        image
+      }
+      keywords {
+        keyword
+      }
+      helpOptions {
+        _id
+        name
+        description
+        numOfPeople
+        registeredUsers {
+          _id
+          username
+        }
+        moneyRequested
+        moneyReceived
+      }
+      comments {
+        author {
+          username
+          image
+          firstName
+          lastName
+        }
+        body
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query Query {
+    me {
+      _id
+      username
+      email
+      firstName
+      lastName
+      firstName
+      image
+      createdBiiggies {
+        _id
+        title
+        deadline
+        description
+        sources
+        images
+        helpOptions {
+          name
+          description
+          numOfPeople
+          moneyRequested
+          moneyReceived
+        }
+      }
+    }
+  }
 `;
