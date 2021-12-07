ced7b1d96ff6344db9d7c6468852152eb68a02de
Carson Johnson <carson74johnson@gmail.com>
2021-12-06 22:13:34 -0500

    added search const to index.js

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -2,3 +2,7 @@ import React, { useEffect } from "react";
 import { useQuery } from '@apollo/client';
 import { GET_BIIGIES, GET_KEYWORDS } from '../../utils/queries.js';
 import { Link } from "react-router-dom";
+
+const Search = () => {
+    
+}
