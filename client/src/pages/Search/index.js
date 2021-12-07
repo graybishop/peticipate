9281c08eab3612f1248c6e8f27ad58cef5b0ee84
Carson Johnson <carson74johnson@gmail.com>
2021-12-06 22:18:03 -0500

    added GET_BIIGGIES and GET_KEYWORDS queries to search index.js

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -4,5 +4,9 @@ import { GET_BIIGIES, GET_KEYWORDS } from '../../utils/queries.js';
 import { Link } from "react-router-dom";
 
 const Search = () => {
-    
+    const { data: biiggiesData, error: biiggiesError } = useQuery(GET_BIIGIES);
+    console.log(biiggiesData);
+
+    const { data: keywordsData, error: keywordsError} = useQuery(GET_KEYWORDS);
+    console.log(keywordsData);
 }
