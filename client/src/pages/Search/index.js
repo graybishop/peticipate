d7ad558583d477724efd3130067a2ca3b1f8a952
Carson Johnson <carson74johnson@gmail.com>
2021-12-06 22:25:06 -0500

    implemented test render page

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 import { useQuery } from '@apollo/client';
 import { GET_BIIGIES, GET_KEYWORDS } from '../../utils/queries.js';
-import { Link } from "react-router-dom";
-import { BiiggieCard } from "../../pages/HomePage/BiiggieCard";
+// import { Link } from "react-router-dom";
+// import { BiiggieCard } from "../../pages/HomePage/BiiggieCard";
 
 const SearchPage = () => {
     const { data: biiggiesData, error: biiggiesError } = useQuery(GET_BIIGIES);
@@ -13,7 +13,12 @@ const SearchPage = () => {
 
     document.title = 'Search || Search Keywords';
 
-    
-}
+
+
+    return (
+
+        <p>Test Render</p>
+    );
+};
 
 export default SearchPage;
