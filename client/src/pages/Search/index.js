d6d518f01eb08cb75e6ec69e40ab7622f9f5b1f5
Carson Johnson <carson74johnson@gmail.com>
2021-12-06 22:20:39 -0500

    added document.title

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -3,12 +3,14 @@ import { useQuery } from '@apollo/client';
 import { GET_BIIGIES, GET_KEYWORDS } from '../../utils/queries.js';
 import { Link } from "react-router-dom";
 
-const Search = () => {
+const SearchPage = () => {
     const { data: biiggiesData, error: biiggiesError } = useQuery(GET_BIIGIES);
     console.log(biiggiesData);
 
     const { data: keywordsData, error: keywordsError} = useQuery(GET_KEYWORDS);
     console.log(keywordsData);
+
+    document.title = 'Search || Search Keywords';
 }
 
-export default Search;
+export default SearchPage;
